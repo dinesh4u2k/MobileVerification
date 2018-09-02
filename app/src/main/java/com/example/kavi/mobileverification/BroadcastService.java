2f49f5e3a11589e4938d3c4d2057f1b12c0300eb
dinesh4u2k <dinesh4u2k@gmail.com>
2018-09-02 14:15:31 +0530

    sep 2

---
+++ app/src/main/java/com/example/kavi/mobileverification/BroadcastService.java
@@ -0,0 +1,46 @@
+package com.example.kavi.mobileverification;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+
+
+public class BroadcastService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        onTaskRemoved(intent);
+        new CountDownTimer(100000,4000)
+        {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                sendBroadcast(new Intent("fromservice"));
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+
+        Intent bcs = new Intent(getApplicationContext(),this.getClass());
+        bcs.setPackage(getPackageName());
+        startService(bcs);
+        super.onTaskRemoved(rootIntent);
+    }
+}
+
